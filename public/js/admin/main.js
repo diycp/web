9043e3d30a7503b082874409abd3518838eac948
Astonep <imland@outlook.com>
2016-10-20 17:50:27 +0800

    update

--- public/js/admin/main.js
+++ public/js/admin/main.js
@@ -1,1 +1,14 @@
-main.js
+/*左侧从菜单*/
+
+$(document).ready(function(){
+
+var extend = 	
+
+$(".admin-menu-parent").click(function(){
+	var $this = $(this);
+	$this.find('a i').toggleClass('fa-angle-down')
+	$this.find('ul').slideToggle("slow");
+  
+
+  });
+});
