75f75b46f481d4b4a3a63008570cb988add7716c
Astonep <imland@outlook.com>
2016-10-21 16:01:16 +0800

    update

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 ### Schedule track
 
 - update admin login/auth 16.9.27
+- update admin menu/auth 16.10.17
--- public/js/admin/main.js
+++ public/js/admin/main.js
@@ -1,20 +1,34 @@
 /*左侧从菜单*/
 
 $(document).ready(function(){
+	$('.admin-menu-parent').find('.admin-menu-dropdown-toggle').click(function() {
+	    var $this = $(this)
+	    var menuList = $this.parent().parent();
+
+	    menuList.find('.admin-menu-parent').each(function(index, value) {
+	        var exists = $(this).find('botton').hasClass('admin-menu-botton')
+	        if (exists) {
+	            $(this).find('botton').removeClass('admin-menu-botton')
+	        }
+	    })
+
+	    $this.find('#toggle-angle').toggleClass('active')
+	    $this.addClass('admin-menu-botton')
+	    $this.siblings('ul').slideToggle('slow')
+	});
+	
+//菜单选中样式
+	$('.sub_button').each(function(){
+		var href = $(this).find('a').attr('href')
+		var loc = String(window.location.pathname)
+		if (href == loc){
+			$(this).parent().siblings('botton').addClass('admin-menu-botton')
+        	$(this).addClass('menu-active')
+		}
+	})
 
 
+ 
 
-$('.admin-menu-parent').click(function(){
-	var $this = $(this);
-	$this.find('a').find('#toggle-angle').toggleClass('fa-angle-down')
 
-	// var extend = $('.admin-menu-child').attr('style');
-	// if (extend != 'display:none;') {
-	// 	extend.slideUp('fast')
-	// }
-
-	$this.find('ul').slideToggle('slow');
-  
-
-  });
 });
